d4faac22b8c5a930dbe8422ea7fa3c944bbfadbc
Arina <yusupovaari@yandex.ru>
2024-11-23 16:31:16 +0200

    add simple ocr

--- character-recognition/tests/test_routes.py
+++ character-recognition/tests/test_routes.py
@@ -6,6 +6,12 @@ def test_config():
     assert create_app({"TESTING": True}).testing
 
 
-def test_hello(client):
-    response = client.get("/hello")
-    assert response.data == b"Hello, World!"
+def test_extract_text(client):
+    with open("tests/data/eng_image.jpg", "rb") as image:
+        data = {"file": (image, "eng_image.jpg")}
+        response = client.post(
+            "/extract-text", content_type="multipart/form-data", data=data
+        )
+
+    assert response.status_code == 200
+    assert "extracted_text" in response.get_json()
