59fe718b349adccce37b64fcb3b920717d5226eb
Arina <yusupovaari@yandex.ru>
2024-12-08 15:57:51 +0200

    fix

--- character-recognition/locustfile.py
+++ character-recognition/locustfile.py
@@ -1,5 +1,4 @@
 from locust import HttpUser, between, task
-from prometheus_client import start_http_server
 
 
 class OCRUser(HttpUser):
@@ -7,13 +6,10 @@ class OCRUser(HttpUser):
 
     @task
     def test_extract_text(self):
-        with open("tests/data/eng_image.jpg", "rb") as img:
+        with open("/mnt/locust/tests/data/eng_image.jpg", "rb") as img:
             files = {"file": ("eng_image.jpg", img, "image/jpeg")}
             response = self.client.post("/extract-text", files=files)
 
         # Ensure the response is correct
         assert response.status_code == 200
         assert response.json() is not None
-
-
-start_http_server(8090)
