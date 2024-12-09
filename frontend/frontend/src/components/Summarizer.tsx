55936f64f7fc0e977e9f833f960db0f4f6255eb3
JulieL999 <julie.lomonosova283@gmail.com>
2024-12-09 18:23:49 +0100

    Created the summarizer

--- frontend/frontend/src/components/Summarizer.tsx
+++ frontend/frontend/src/components/Summarizer.tsx
@@ -1,5 +1,35 @@
+import Textareas from "./Textareas";
+import Footer from "./Footer";
+
 function Summarizer() {
-  return <h1>Summarizer</h1>;
+  return (
+    <div className="container-fluid vh-100 d-flex flex-column">
+      <h1>Summarizer</h1>
+      <div className="d-flex align-items-center gap-2">
+        <p>Style: </p>
+        <button type="button" className="btn btn-outline-success">
+          Detailed
+        </button>
+        <button type="button" className="btn btn-outline-success">
+          Short
+        </button>
+      </div>
+      <Textareas />
+      <div className="d-flex align-items-center gap-2">
+        <button type="button" className="btn btn-outline-secondary">
+          Upload Image
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Upload Audio
+        </button>
+        <button type="button" className="btn btn-success">
+          Summarize
+        </button>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Summarizer;
---
+++ frontend/frontend/src/components/Textareas.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const Textareas = () => {
+  const [text, setText] = useState("");
+
+  const handleClear = () => {
+    setText("");
+  };
+
+  return (
+    <div className="d-flex mb-3 mt-2 vh-100">
+      <div className="w-50 me-2 d-flex position-relative">
+        <div data-mdb-input-init className="form-outline flex-grow-1">
+          <textarea
+            className="form-control h-100"
+            id="input"
+            placeholder="To rewrite text, enter or paste it here and press Paraphraze"
+            style={{ resize: "none" }}
+            value={text} // Bind the value to the state
+            onChange={(e) => setText(e.target.value)} // Update state on change
+          />
+
+          <div
+            className="position-absolute top-0 end-0 mt-2 me-2 bg-light p-1"
+            style={{ cursor: "pointer", fontSize: "1em" }}
+            onClick={handleClear} // Add click handler
+          >
+            <i className="bi bi-x-circle"></i>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-50 ms-2 d-flex">
+        <div data-mdb-input-init className="form-outline flex-grow-1">
+          <textarea
+            className="form-control h-100"
+            id="output"
+            readOnly
+            style={{ resize: "none" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Textareas;
