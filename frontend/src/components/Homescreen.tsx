5853e5c95625a473a793e6e0d6632bd182019e67
JulieL999 <julie.lomonosova283@gmail.com>
2025-01-12 20:19:20 +0100

    Integrated image input

--- frontend/src/components/Homescreen.tsx
+++ frontend/src/components/Homescreen.tsx
@@ -1,16 +1,53 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Textareas from "./Textareas";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import axios from "axios";
 
 const Homescreen: React.FC = () => {
   let title = "Study Companion";
   const [text, setText] = useState("");
   const [resultText, setResultText] = useState("");
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  // Function to trigger the file input dialog
+  const triggerImageUpload = () => {
+    if (imageInputRef.current) {
+      imageInputRef.current.click();
+    }
+  };
+
+  const handleImageUpload = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      try {
+        const response = await axios.post(
+          "http://character-recognition:5001/extract-text",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+
+        setText((prevText) => prevText + "\n\n" + response.data.extracted_text);
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        alert("An unexpected error occurred. Please try again.");
+      }
+    }
+  };
 
   return (
     <div className="container-fluid vh-100 d-flex flex-column">
       <Header title={title} />
+      <hr className="border border-1 opacity-50"></hr>
       <Textareas
         text={text}
         setText={setText}
@@ -57,7 +94,14 @@ const Homescreen: React.FC = () => {
             </button>
             <ul className="dropdown-menu">
               <li>
-                <a className="dropdown-item" href="#">
+                <a
+                  className="dropdown-item"
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault(); // Prevent default navigation behavior
+                    triggerImageUpload(); // Trigger the file input dialog
+                  }}
+                >
                   Image
                 </a>
               </li>
@@ -67,6 +111,13 @@ const Homescreen: React.FC = () => {
                 </a>
               </li>
             </ul>
+            <input
+              id="upload-image"
+              ref={imageInputRef} // Assign the ref to the input
+              style={{ display: "none" }}
+              type="file"
+              onChange={handleImageUpload} // Handle the file selection
+            />
           </div>
         </div>
         <div style={{ flex: "2" }} className="ms-2 d-flex justify-content-end">
