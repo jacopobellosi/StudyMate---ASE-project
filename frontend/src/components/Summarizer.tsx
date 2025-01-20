f505d6d3a24a1a75fabf070d9cccad47165093ff
jacopobellosi <j.bellosi@studenti.unibg.it>
2025-01-20 23:43:44 +0100

    should be working
    i have modified the Docker File because i don't why it wasn't working

--- frontend/src/components/Summarizer.tsx
+++ frontend/src/components/Summarizer.tsx
@@ -1,10 +1,28 @@
 import TextProcessor from "./TextProcessor";
 import React from "react";
 
-const SummarizeProcessor: React.FC = () => {
-  const handleAction = async (text: string): Promise<string> => {
-    return 'TODO Summarized text: ' + text;
+const SummarizeProcessor: React.FC = (): JSX.Element => {
+
+
+  //get the value of the textarea id=input in the TextProcessor component
+  const getInputValue = (): string => {
+    const inputElement = document.getElementById('input') as HTMLTextAreaElement;
+    return inputElement ? inputElement.value : '';
   };
+const handleAction = async (): Promise<string> => {
+     const response = await fetch(`http://localhost:8000/summarize?request=${getInputValue()}&percentage=70`);
+      console.log(response);
+    if (!response.ok) {
+        throw new Error('Failed to summarize text');
+    }
+
+    const summarizedText = await response.json();
+    const outputElement = document.getElementById('output') as HTMLTextAreaElement;
+    if (outputElement) {
+      outputElement.value = summarizedText;
+    }
+    return summarizedText
+    };
 
   return (
     <TextProcessor
